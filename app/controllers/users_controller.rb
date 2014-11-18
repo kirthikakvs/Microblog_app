18c192a1ec06dc18d5b34572732181142abb5b97
kirthika <kirthika@freshdesk.com>
2014-11-18 08:48:37 +0530

    Finished login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,11 @@ class UsersController < ApplicationController
   	def create
     	@user = User.new(params[:user])
     	if @user.save
-          flash[:success] = "Welcome to the Microblog App!!"
-      		redirect_to @user
-    	else
-      		render 'new'
-    	end
+        sign_in @user
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to @user
+      else
+        render 'new'
+      end
   	end
 end
